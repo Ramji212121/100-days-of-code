9854dac063bca6a473553d43ffeb54eea52d1e7d
Ramji212121 <ramjigupta5970@gmail.com>
2025-09-08 12:32:09 +0530

    Create product.cpp

---
+++ product.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, sum,difference,product;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    sum = a + b;
+    printf("Sum = %d\n", sum);
+    difference = a-b;
+    printf("difference=%d\n", difference);
+    product = a*b;
+    printf("product=%d\n", product);
+    
+
+    return 0;
+}
+
