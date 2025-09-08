9fa8afa9b7b39a24bfb0d09973d709148e9d806c
Ramji212121 <ramjigupta5970@gmail.com>
2025-09-08 12:37:50 +0530

    Create age.cpp

---
+++ age.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+	int age,year,sum;
+	printf ("enter the age:");
+	scanf ("%d", &age);
+	printf("enter the year:");
+	scanf ("%d", &year);
+	 sum =age + year;
+	printf("sum is %d\n", sum);
+	
+	return 0;
+}
