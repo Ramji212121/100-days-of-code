0c9ce9245afa3e8326b599b0e5b87fe0e7b396b3
Ramji212121 <ramjigupta5970@gmail.com>
2025-10-04 00:40:33 +0530

    100 days of code

---
+++ day36q2.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    int rows, cols;
+    int i, j, sum = 0;
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+    printf("Enter number of columns: ");
+    scanf("%d", &cols);
+
+    int matrix[rows][cols];
+    printf("Enter elements of the matrix:\n");
+    for(i = 0; i < rows; i++) {
+        for(j = 0; j < cols; j++) {
+            printf("Element [%d][%d]: ", i+1, j+1);
+            scanf("%d", &matrix[i][j]);
+            sum += matrix[i][j];  
+        }
+    }
+
+    printf("\nThe Matrix is:\n");
+    for(i = 0; i < rows; i++) {
+        for(j = 0; j < cols; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\nSum of all elements = %d\n", sum);
+
+    return 0;
+}
+
