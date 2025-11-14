4d5a5dd204af3220fcbf38cfda27e3dc7f71c0b9
Ramji212121 <ramjigupta5970@gmail.com>
2025-11-14 16:48:21 +0530

    100 days of code

---
+++ day 44 q2.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    char str[200];
+    int i;
+
+    printf("Enter a string: ");
+    gets(str);  
+
+    for (i = 0; str[i] != '\0'; i++) {
+        if (str[i] == ' ') {
+            str[i] = '-';   
+        }
+    }
+
+    printf("String after replacing spaces with hyphens: %s\n", str);
+
+    return 0;
+}
+
